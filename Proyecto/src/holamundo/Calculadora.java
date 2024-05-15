1d08b3891e12cfeb30fe63641e433366d44d4b88
JanoT19 <janotoralsanmiguel2004@gmail.com>
2024-05-15 12:12:14 +0200

    cambio

--- Proyecto/src/holamundo/Calculadora.java
+++ Proyecto/src/holamundo/Calculadora.java
@@ -1,9 +1,11 @@
 package holamundo;
 
-public class Helloworld {
+public class Calculadora {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		System.out.println("");
 
 	}
 
---
+++ Proyecto/src/holamundo/Operaciones.java
@@ -0,0 +1,21 @@
+package holamundo;
+
+import java.text.DecimalFormat;
+
+import javax.swing.JOptionPane;
+
+public class Operaciones {
+	
+	public static void multiplicar() {
+		DecimalFormat df = new DecimalFormat("#.##");
+		int num1 = Integer.parseInt(JOptionPane.showInputDialog("Escribe el primer numero"));
+		int num2 = Integer.parseInt(JOptionPane.showInputDialog("Escribe el segundo numero"));
+		float resultado = num1*num2;
+		JOptionPane.showMessageDialog(null, num1 + " x " + num2 + " = " + df.format(resultado));
+	}
+	
+	public static void main(String[] args) {
+		multiplicar();
+	}
+
+}
