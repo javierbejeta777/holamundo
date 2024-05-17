751a612fd6d7452125cc832e883d778b8c2878aa
Jano Toral <166606707+JanoTo19@users.noreply.github.com>
2024-05-17 17:26:06 +0200

    Update Multiplicar.java

--- Proyecto/src/holamundo/Multiplicar.java
+++ Proyecto/src/holamundo/Multiplicar.java
@@ -5,11 +5,15 @@ import java.text.DecimalFormat;
 import javax.swing.JOptionPane;
 
 public class Multiplicar {
-	
-	public static void Multiplicar() {
+
+	public static void main(String[] args) {
 		DecimalFormat df = new DecimalFormat("#.##");
 		int num1 = Integer.parseInt(JOptionPane.showInputDialog("Escribe el primer numero"));
 		int num2 = Integer.parseInt(JOptionPane.showInputDialog("Escribe el segundo numero"));
+		Multiplicar(num1,num2);
+	}
+	
+	public static void Multiplicar(int num1, int num2) {
 		float resultado = num1*num2;
 		JOptionPane.showMessageDialog(null, num1 + " x " + num2 + " = " + df.format(resultado));
 	}
