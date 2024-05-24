ef5bc00728938f705e63ecbd550fc34944675043
Jano Toral <jtoralsm@ceinmark.net>
2024-05-24 20:41:28 +0200

    cambios

--- Proyecto/src/holamundo/Dividir.java
+++ Proyecto/src/holamundo/Dividir.java
@@ -3,14 +3,19 @@ package holamundo;
 import java.util.Scanner;
 
 public class Dividir {
-	public static void main (String args) {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 		Scanner ent=new Scanner(System.in);
+		System.out.println("Escribe el primer numero");
 		String num1= ent.nextLine();
+		System.out.println("Escribe el segundo numero");
 		String num2= ent.nextLine();
-		Division(Double.parseDouble(num1),Double.parseDouble(num2));
+		System.out.println(Division(Double.parseDouble(num1),Double.parseDouble(num2)));
 	}
-public static double Division(double a,double b) {
-	return a/b;
-}
+	
+	public static double Division(double a,double b) {
+		return a/b;
+	}
 
 }
